9228fa799fb2a19d673ef4ef5ec490795a7a58dc
Alexey Nikitin <nikitin@bookmate.com>
2018-11-19 19:38:58 +0300

    process entries

--- src/eep48.erl
+++ src/eep48.erl
@@ -1,45 +1,52 @@
 -module(eep48).
 -export([
+    file_path/0,
     edoc/0,
-    to_md/1,
-    to_xml/1,
-    doc_tag/1,
-    file_path/0
+    edoc/1
 ]).
 
 -include_lib("edoc/src/edoc.hrl").
 % rr(filename:join(code:lib_dir(edoc, src), "edoc.hrl")).
 % rr(filename:join(code:lib_dir(xmerl, include), "xmerl.hrl")).
 
+file_path() ->
+    filename:join(code:priv_dir(?MODULE), "riak_client.erl").
+
 edoc() ->
-    Src = file_path(),
+    edoc(file_path()).
+
+edoc(Src) ->
     Opts = [],
     Env = edoc_lib:get_doc_env(Opts),
     {_Module, Data} = eep48_extract:source(Src, Env, Opts),
+    entries(Data).
+
+entries(Data) ->
     lists:filtermap(fun entry/1, Data).
 
 entry(#entry{name = {_Name, _Arity}} = Entry) ->
-    {true, Entry};
+    {true, process_entry(Entry)};
 entry(_) ->
     false.
 
-doc_tag(#entry{data = Data}) ->
-    [DocTag] = lists:filtermap(fun eep48:doc_tag/1, Data),
-    DocTag;
-doc_tag(#tag{name = doc, data = Data}) ->
-    {true, Data};
-doc_tag(_) ->
-    false.
+process_entry(#entry{name = {Name, Arity}, line = Line, args = Args, data = Data}) ->
+    {
+        {function, Name, Arity},
+        Line,
+        signature(Name, Args),
+        #{ <<"en">> => doc_string(Data) },
+        #{}
+    }.
 
-to_md(Xml) when is_list(Xml) ->
-    io:format("~s~n", [xmerl:export_simple(Xml, edown_xmerl)]);
-to_md(Xml) ->
-    to_md([Xml]).
+signature(Name, ArgList) ->
+    Args = lists:join(", ", [atom_to_list(A) || A <- ArgList]),
+    iolist_to_binary(io_lib:format("~s(~s)", [Name, Args])).
 
-to_xml(Xml) when is_list(Xml) ->
-    io:format("~s~n", [xmerl:export_simple(Xml, xmerl_xml)]);
-to_xml(Xml) ->
-    to_xml([Xml]).
+doc_string(Data) ->
+    iolist_to_binary(
+        lists:map(fun tag_to_markdown/1, Data)).
 
-file_path() ->
-    filename:join(code:priv_dir(?MODULE), "riak_client.erl").
+tag_to_markdown(#tag{name = doc, data = Xml}) ->
+    xmerl:export_simple(Xml, edown_xmerl);
+tag_to_markdown(_) ->
+    "".
